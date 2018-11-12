de3bc753776b0b3d0af14dc23c6d2fc8d649849d
kaocher <kaocher82@gmail.com>
2018-11-13 00:25:49 +0600

    Angular framework

--- root/home/views.py
+++ root/home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from django.http import Http404
+
+
 # Create your views here.
 from .models import Brands
 from .models import Phones
@@ -8,10 +13,15 @@ def index(request):
     context = {
         "brand_list": Brands.objects.all()
     }
+
     return render(request, "index.html", context)
 
-def indexPhone(request):
+def BrandPhone(request, Brands_id):
+    try:
+        Brand_phone = Phones.object.get(pk=Brands_id)
+    except Brands.DoseNotExist:
+        raise Http404(" Brands Dose Not Exist ")
     context = {
-        "Phone_list": Phones.objects.all()
+        "Phone_list": phones
     }
-    return render(request, "index.html", context)
+    return render(request, "brand.html", context)
